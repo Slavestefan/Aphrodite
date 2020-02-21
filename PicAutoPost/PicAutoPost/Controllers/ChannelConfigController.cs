3d564fd8c2db38c5c73ad85dd9793a66ff16a4a6
slavestefan <slavestefan@not-a-real-email.com>
2020-02-21 23:19:40 +0100

    First basic UI stuff

---
+++ PicAutoPost/PicAutoPost/Controllers/ChannelConfigController.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System;
+using Microsoft.AspNetCore.Mvc;
+using Slavestefan.Aphrodite.Model;
+
+namespace Slavestefan.Aphrodite.Web.Controllers
+{
+    public class ChannelConfigController : Controller
+    {
+        private readonly PicAutoPostContext _context;
+
+        public ChannelConfigController(PicAutoPostContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ PicAutoPost/PicAutoPost/Controllers/DummyController.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Slavestefan.Aphrodite.Web.Controllers
+{
+    /// <summary>
+    /// This is just a dummy controller that points to an empty page for the bot's keep alive.
+    /// </summary>
+    public class DummyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
