0a78dcb388d07ba4f3936c06c3598a6e9335e446
slavestefan <slavestefan@not-a-real-email.com>
2020-04-28 23:52:38 +0200

    Bugfix: Missing migration

---
+++ PicAutoPost/Model/Migrations/20200428202601_v8.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Slavestefan.Aphrodite.Model.Migrations
+{
+    public partial class v8 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Users");
+        }
+    }
+}
